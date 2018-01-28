84df8f5dfa0b07743f6224560c194f12d1d16c54
Alessandro Cocco <alessandro.cocco@outlook.com>
2018-01-28 22:30:55 +0100

    Removed uselss css & js

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,8 +1,5 @@
 
 $(document).ready(function() {
-    // This cookie discaimer is a fork of stiank's discaimer - https://www.bootply.com/stiank/2E0oBIpqlu
-    handleCookie();
- 
     scrollToTopHandler();
 
     initGoodreads();
@@ -11,8 +8,6 @@ $(document).ready(function() {
     function initGoodreads() {
         noBookMessage('alessandro');
         noBookMessage('elisa');
-        // fixGoodreadsLink('alessandro', 'https://www.goodreads.com/user/show/21784726-alessandro');
-        // fixGoodreadsLink('elisa', 'https://www.goodreads.com/user/show/35167135-elisa');
     }
 
     function noBookMessage(name) {
@@ -22,63 +17,10 @@ $(document).ready(function() {
         }
     }
 
-    // function fixGoodreadsLink(who, url) {        
-    //     $('#gr_custom_widget_' + who + ' > div > center > a').attr('href', url);
-    //     $('#gr_custom_widget_' + who + ' > div > center > a').attr('target', '_blank');
-    // }
-    /* Goodreads - END */
-
     function scrollToTopHandler() {
         $('.top-link').on('click', function(e) {
             e.preventDefault();
             $('html, body').animate({scrollTop: 0}, 750);
         });
-    }
-
-    function handleCookie() {
-        var cookie = false;
-        var cookieContent = $('.cookie-disclaimer');
-
-        checkCookie();
-
-        if (cookie === false) {
-            cookieContent.slideDown();
-        }
-
-        function checkCookie() {
-            var check = getCookie("acookie");
-            if (check !== "") {
-                return cookie = true;
-            } else {
-                return cookie = false;
-            }
-        }
-
-        function getCookie(cname) {
-            var name = cname + "=";
-            var ca = document.cookie.split(';');
-
-            for (var i = 0; i < ca.length; i++) {
-                var c = ca[i].trim();
-                if (c.indexOf(name) === 0) {
-                    return c.substring(name.length, c.length);
-                }
-            }
-
-            return "";
-        }
-
-        $('.accept-cookie').click(function() {
-            setCookie("acookie", "accepted", 365);
-            cookieContent.slideUp();
-        });
-
-        function setCookie(cname, cvalue, exdays) {
-            var d = new Date();
-            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-
-            var expires = "expires=" + d.toGMTString();
-            document.cookie = cname + "=" + cvalue + "; " + expires;
-        }
     }
 });
